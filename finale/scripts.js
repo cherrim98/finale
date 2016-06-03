7011e16b56af90ba2f6c77f2f73b2fe49cadc517
Aaron <cherrim98@gmail.com>
2016-06-03 10:37:40 -0400

    Add files via upload

--- finale/scripts.js
+++ finale/scripts.js
@@ -2,3 +2,14 @@ $('#myTabs a').click(function (e) {
   e.preventDefault()
   $(this).tab('show')
 })
+
+/* Scripts for wave effect parallax and scrollspy demo */
+
+$(document).ready(function(){
+
+    /* For parallaxing images */
+    $('.parallax').parallax();
+
+    /* To scroll to a section from navbar */
+    $('.scrollspy').scrollSpy();
+});
